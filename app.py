a3e2424dd8ed52551a0692a3cac034a948e3d1a6
jingyi-ju <96923360+jingyi-ju@users.noreply.github.com>
2022-04-06 21:54:04 -0400

    web app file

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import numpy as np
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import pickle
 from keras.preprocessing import image
-from PIL import Image
-from numpy import asarray
-import keras
-import tensorflow as tf
+
 
 with open('model_pkl', 'rb') as f:
     cnn_model = pickle.load(f)
@@ -23,7 +20,7 @@ def get_image():
     classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
                'V', 'W', 'X', 'Y']
     if request.method == 'POST':
-        img = request.files['my_image']
+        img = request.files['image']
         img_path = "static/" + img.filename
         img.save(img_path)
         p = cnn_model.predict(np.reshape(np.asarray(image.load_img(img_path, color_mode="grayscale")), (1, 28, 28, 1)))
@@ -32,4 +29,4 @@ def get_image():
     return render_template("index.html", prediction=classes[index], img_path=img_path)
 
 if __name__ =='__main__':
-	app.run(debug = True)
+	app.run()
